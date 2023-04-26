f1e8e865b99cd9ba59d4909f43bea77c3d88af72
Jakub Kadlcik <frostyx@email.cz>
2023-04-26 22:31:50 +0200

    Starting to work on a modal dialog for GitHub profiles

--- src/cockpit_ssh_keys/core.cljs
+++ src/cockpit_ssh_keys/core.cljs
@@ -12,16 +12,21 @@
 (def users
   (r/atom [{:login "FrostyX"
             :avatar_url "https://avatars.githubusercontent.com/u/2151771?v=4"
-            :html_url "https://github.com/FrostyX"}
+            :html_url "https://github.com/FrostyX"
+            :url "https://api.github.com/users/FrostyX"}
            {:login "frostyx-vk"
             :avatar_url "https://avatars.githubusercontent.com/u/115023317?v=4"
-            :html_url "https://github.com/frostyx-vk"}
+            :html_url "https://github.com/frostyx-vk"
+            :url "https://api.github.com/users/frostyx-vk"}
            {:login "Frostyx0"
             :avatar_url "https://avatars.githubusercontent.com/u/56936540?v=4"
-            :html_url "https://github.com/Frostyx0"}
+            :html_url "https://github.com/Frostyx0"
+            :url "https://api.github.com/users/Frostyx0"}
            {:login "Frostyxnova"
             :avatar_url "https://avatars.githubusercontent.com/u/90808223?v=4"
-            :html_url "https://github.com/Frostyxnova"}]))
+            :html_url "https://github.com/Frostyxnova"
+            :url "https://api.github.com/users/Frostyxnova"}]))
+
 
 ;; Searched login
 (def login (r/atom ""))
@@ -29,19 +34,31 @@
 ;; List of authorized keys for the current user
 (def authorized-keys (r/atom []))
 
+(def dialog-for-user (r/atom nil))
+(def full-user (r/atom nil))
+
 
 (defn search-github-user [login]
   (-> (fetch/get (str "https://api.github.com/search/users?q=" login)
                  {:accept :json :content-type :json})
       (.then (fn [resp] (-> resp :body (gobj/get "items"))))
       (.then (fn [items]
+               (js/console.log items)
                (gobj/forEach
                 items
                 (fn [val key items]
                   (swap! users conj
                          {:login (gobj/get val "login")
                           :avatar_url (gobj/get val "avatar_url")
-                          :html_url (gobj/get val "html_url")})))))))
+                          :html_url (gobj/get val "html_url")
+                          :url (gobj/get val "html_url")})))))))
+
+
+(defn query-full-github-user [user]
+  (-> (fetch/get (:url user) {:accept :json :content-type :json})
+      (.then (fn [resp] (-> resp :body (js->clj :keywordize-keys true))))
+      (.then (fn [user]
+               (reset! full-user user)))))
 
 
 (defn get-pubkeys [user]
@@ -82,6 +99,53 @@
             :on-click #(search-github-user @login)}]])
 
 
+;; https://api.github.com/users/FrostyX
+(defn render-modal-profile-body []
+  (query-full-github-user @dialog-for-user)
+  (when @full-user
+  (let [user @full-user]
+    [:div
+     [:img {:src (:avatar_url user) :width 150 :height 150}]
+     [:p (:name user)]
+     [:p [:i {:class ""}] (:login user)]
+     [:p [:i {:class "pf-icon pf-icon-enterprise"}] (:company user)]
+     [:p [:i {:class "fas fa-external-link-alt"}] (:blog user)]
+     [:p [:i {:class "fas fa-map-marker"}] (:location user)]
+     [:p [:i {:class ""}] (:login user)]
+     [:p [:i {:class "pf-icon pf-icon-repository"}] (:public_repos user)]
+     [:p (:public_gists user)]
+     [:p [:i {:class "fas fa-user-friends"}] (:followers user)]
+     [:p (:following user)]
+     [:p [:i {:class "fas fa-key"}] "TODO Public key"]
+     [:p (:html_url user)]])))
+
+
+(defn render-modal-profile []
+  (when @dialog-for-user
+    [:div {:class "pf-c-backdrop"}
+     [:div {:class "pf-l-bullseye"}
+      [:div {:class "pf-c-modal-box pf-m-sm"
+             :role "dialog"
+             :aria-modal "true"
+             :aria-labelledby "modal-title-modal-basic-example-modal"
+             :aria-describedby "modal-description-modal-basic-example-modal"}
+
+       [:header {:class "pf-c-modal-box__header"}
+        [:h1 {:class "pf-c-modal-box__title"
+              :id "modal-title-modal-basic-example-modal"}
+         "Authorize this user?"]]
+       [:div {:class "pf-c-modal-box__body"
+              :id "modal-description-modal-basic-example-modal"}
+        (render-modal-profile-body)]
+
+       [:footer {:class "pf-c-modal-box__footer"}
+        [:button {:class "pf-c-button pf-m-primary" :type "button"} "Add SSH key"]
+        [:button {:class "pf-c-button pf-m-link"
+                  :type "button"
+                  :on-click #(reset! dialog-for-user nil)}
+         "Cancel"]]]]]))
+
+
 (defn render-table []
   [:table {:role "grid"
            :class "ct-table pf-c-table pf-m-grid-md pf-m-compact"}
@@ -89,19 +153,23 @@
     [:tr
      [:th {:scope "col"} "Avatar"]
      [:th {:scope "col"} "Username"]
-     [:th {:scope "col"} "Enable"]]]
+     [:th {:scope "col"} "Enable"]
+     [:th {:scope "col"} "GitHub URL"]]]
    [:tbody {:role "rowgroup"}
     (for [user @users]
-    [:tr
-     [:td {} [:img {:src (:avatar_url user) :width 50 :height 50}]]
-     [:td {} [:a {:href (:html_url user)} (:login user)]]
-     [:td {} "Allow this user"]])]])
+      [:tr
+       [:td {} [:img {:src (:avatar_url user) :width 50 :height 50}]]
+       [:td {} [:a {:href (:html_url user)} (:login user)]]
+       [:td {} [:button {:on-click #(reset! dialog-for-user user)}
+                "Allow this user"]]
+       [:td {} [:a {:href (:html_url user)} (:html_url user)]]])]])
 
 
 (defn home-page []
   (load-pubkeys)
 
   [:div [:h2 "Welcome to Reagent"]
+   (render-modal-profile)
    (render-form)
    (render-table)])
 
